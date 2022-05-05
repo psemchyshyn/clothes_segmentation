3bb4b3993f3cd42089c21cd544cfcf1a5253cfc9
Pavlo Semchyshyn <pavlosemchyshyn@gmail.com>
2022-05-05 09:08:46 +0300

    ch

--- lightining.py
+++ lightining.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn.functional as F
 import pytorch_lightning as pl
 import torchmetrics
-from torchvision.utils import save_image
 import matplotlib.pyplot as plt
 from functools import partial
 from unet.model import UNet
@@ -26,7 +25,7 @@ class LitSegClothes(pl.LightningModule):
     def forward(self, x):
         return self.model(x)
 
-    def training_step(self, batch, batch_idx):
+    def step(self, batch, batch_idx, type):
         x, mask = batch
         x = x.float()
         mask = mask.long()
@@ -34,31 +33,23 @@ class LitSegClothes(pl.LightningModule):
         loss = F.cross_entropy(y, mask, ignore_index=self.num_classes)
 
         for key, metric in self.metrics.items():
-            self.log(f"train_{key}", metric(y, mask))
-        self.log('train_loss', loss)
+            self.log(f"{type}_{key}", metric(y, mask))
+        self.log(f'{type}_loss', loss)
 
         return loss
+
+    def training_step(self, batch, batch_idx):
+        return self.step(batch, batch_idx, "train")
 
     def validation_step(self, batch, batch_idx):
-        x, mask = batch
-        x = x.float()
-        mask = mask.long()
-
-        y = self(x)
-        loss = F.cross_entropy(y, mask, ignore_index=self.num_classes)
-        self.on_train_epoch_end()
-        for key, metric in self.metrics.items():
-            self.log(f"val_{key}", metric(y, mask))
-        self.log("val_loss", loss)
-        
-        return loss
+        return self.step(batch, batch_idx, "val")
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         sch = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
         return [optimizer], [sch]
 
-    def on_train_epoch_end(self):
+    def on_validation_epoch_end(self):
         x, _ = next(iter(self.trainer.val_dataloaders[0]))
         x = x.float()
 
